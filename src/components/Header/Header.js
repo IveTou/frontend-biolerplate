c8ce3489ae65fb6cf38abc667758b4382dda12cf
IveTou <ive_tourinho@hotmail.com>
2020-02-19 12:00:47 -0300

    Install and configure linters, storybook and StyledComponents
    - Configure Husky and Jest parameters
    - Install and configure standard
    - Install and configure Storybook
    - Install and configure Styled Components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = () => <S.Header>Header</S.Header>
+
+const S = {
+  Header: styled.header`
+    display: flex;
+    width: 100%;
+  `
+}
+
+export default Header
